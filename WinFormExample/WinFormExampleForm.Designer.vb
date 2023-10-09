41709c1174b3fbfa47d23ec95502bbb1050fea66
tim <rosstimo@isu.edu>
2023-10-09 08:49:59 -0600

    add form control examples

--- WinFormExample/WinFormExampleForm.Designer.vb
+++ WinFormExample/WinFormExampleForm.Designer.vb
@@ -22,17 +22,77 @@ Partial Class WinFormExampleForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ExampleTextBox = New System.Windows.Forms.TextBox()
+        Me.ExampleLabel = New System.Windows.Forms.Label()
+        Me.ActionButton = New System.Windows.Forms.Button()
+        Me.StringReverseCheckBox = New System.Windows.Forms.CheckBox()
         Me.SuspendLayout()
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(625, 344)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(163, 94)
+        Me.ExitButton.TabIndex = 0
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ExampleTextBox
+        '
+        Me.ExampleTextBox.Location = New System.Drawing.Point(134, 12)
+        Me.ExampleTextBox.Name = "ExampleTextBox"
+        Me.ExampleTextBox.Size = New System.Drawing.Size(500, 26)
+        Me.ExampleTextBox.TabIndex = 1
+        '
+        'ExampleLabel
+        '
+        Me.ExampleLabel.AutoSize = True
+        Me.ExampleLabel.Location = New System.Drawing.Point(58, 12)
+        Me.ExampleLabel.Name = "ExampleLabel"
+        Me.ExampleLabel.Size = New System.Drawing.Size(70, 20)
+        Me.ExampleLabel.TabIndex = 2
+        Me.ExampleLabel.Text = "Example"
+        '
+        'ActionButton
+        '
+        Me.ActionButton.Location = New System.Drawing.Point(456, 344)
+        Me.ActionButton.Name = "ActionButton"
+        Me.ActionButton.Size = New System.Drawing.Size(163, 94)
+        Me.ActionButton.TabIndex = 3
+        Me.ActionButton.Text = "&Do The Thing"
+        Me.ActionButton.UseVisualStyleBackColor = True
+        '
+        'StringReverseCheckBox
+        '
+        Me.StringReverseCheckBox.AutoSize = True
+        Me.StringReverseCheckBox.Location = New System.Drawing.Point(134, 44)
+        Me.StringReverseCheckBox.Name = "StringReverseCheckBox"
+        Me.StringReverseCheckBox.Size = New System.Drawing.Size(94, 24)
+        Me.StringReverseCheckBox.TabIndex = 4
+        Me.StringReverseCheckBox.Text = "Reverse"
+        Me.StringReverseCheckBox.UseVisualStyleBackColor = True
         '
         'WinFormExampleForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.StringReverseCheckBox)
+        Me.Controls.Add(Me.ActionButton)
+        Me.Controls.Add(Me.ExampleLabel)
+        Me.Controls.Add(Me.ExampleTextBox)
+        Me.Controls.Add(Me.ExitButton)
         Me.Name = "WinFormExampleForm"
         Me.Text = "Fall 2023 Example"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ExampleTextBox As TextBox
+    Friend WithEvents ExampleLabel As Label
+    Friend WithEvents ActionButton As Button
+    Friend WithEvents StringReverseCheckBox As CheckBox
 End Class
--- WinFormExample/WinFormExampleForm.vb
+++ WinFormExample/WinFormExampleForm.vb
@@ -1,3 +1,15 @@
 ﻿Public Class WinFormExampleForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ActionButton_Click(sender As Object, e As EventArgs) Handles ActionButton.Click
+        Me.Text = ExampleTextBox.Text
+        'reverse only if reverse check box is checked
+        If StringReverseCheckBox.Checked Then
+            ExampleTextBox.Text = StrReverse(ExampleTextBox.Text)
+        End If
+
+    End Sub
 
 End Class
